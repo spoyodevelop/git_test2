aebaa2956af7e547a5ae007b1bbe4d882c28ae8a
spoyodevelop <kjy9505@gmail.com>
2021-09-17 07:02:22 +0900

    Hello 클래스에 message 멤버 변수 추가

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,9 +1,12 @@
 package git_test2;
 
 public class Hello {
+    
+    String message = "æ»≥Á«œººø‰";
 
     public void say() {
-        System.out.println("æ»≥Á«œººø‰");
+        System.out.println(message);
     }
 
 }
+
